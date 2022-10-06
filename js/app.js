4b1232cd62f5e2adc862e3458fa113fb65c054bd
HazemLenin <hazemlenin@gmail.com>
2022-10-06 03:53:37 +0200

    updated active section technique

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-let nanvBar = document.getElementById('navbar');
-let sections = document.getElementsByTagName('section');
+let nanvBar = document.getElementById("navbar");
+let sections = document.getElementsByTagName("section");
 
 // Fill the navbar
 for (let section of sections) {
-	let title = section.getAttribute('data-title');
+	let title = section.getAttribute("data-title");
 	nanvBar.innerHTML += `
         <li>
             <button class="nav-btn" data-section="${title}">${title}</button>
@@ -12,32 +12,38 @@ for (let section of sections) {
 }
 
 // get created navbar buttons and add click listeners to them
-let buttons = document.getElementsByClassName('nav-btn');
+let buttons = document.getElementsByClassName("nav-btn");
 
 for (let button of buttons) {
-	button.addEventListener('click', () => {
+	button.addEventListener("click", () => {
 		for (let section of sections) {
-			 // Scroll into the specefix section with the same title
-			if (section.getAttribute('data-title') === button.getAttribute('data-section')) {
-				section.scrollIntoView({behavior: 'smooth'});
+			// Scroll into the specefix section with the same title
+			if (
+				section.getAttribute("data-title") ===
+				button.getAttribute("data-section")
+			) {
+				section.scrollIntoView({ behavior: "smooth" });
 			}
 		}
 	});
 }
 
 // Add active class to sections in the viewport
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
+	// remove all active classes
+	for (let section of sections) {
+		section.classList.remove("active");
+	}
 	for (let section of sections) {
 		let rect = section.getBoundingClientRect();
 		let viewportHeight = window.innerHeight;
 		if (
-			(rect.top >= 0 && rect.top <= viewportHeight) && // if top is in the viewport 
-			(rect.bottom <= viewportHeight && rect.bottom >= 0) && // if bottom is in the viewport
-			!section.classList.contains('active') // if the section isn't active
+			rect.top >= 0 &&
+			rect.top <= viewportHeight && // if top is in the viewport
+			!section.classList.contains("active") // if the section isn't active
 		) {
-			section.classList.add('active');
-		} else {
-			section.classList.remove('active');
+			section.classList.add("active");
+			break;
 		}
 	}
 });
